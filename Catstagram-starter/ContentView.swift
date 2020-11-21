2963709f04e38b2650411ab01764059988d62aae
Mila Alfitri <alfitriv@gmail.com>
2020-11-21 22:09:25 +0800

    Initial Commit

---
+++ Catstagram-starter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Catstagram-starter
+//
+//  Created by Vania Radmila Alfitri on 21/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
